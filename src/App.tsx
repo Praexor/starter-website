9f705bf86842ae729bcd3090550c3b0e1530ef7a
Praexor <praexor@hotmail.com>
2020-07-24 14:29:32 -0400

    Text added on idex

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOW
+          Learn React 
         </a>
+        <p>NOW</p>
       </header>
     </div>
   );
